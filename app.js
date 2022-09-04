17ff3d7973d3f5160776d0f5188d6eeef6788832
Fred Snyder <safe.sand1626@fastmail.com>
2022-09-04 19:34:32 +0000

    Scaffold logic

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 // app
 console.log("Tennis Player Salaries")
+
+// sample data
+const matches = [
+    {
+        "playerId": 1,
+        "opponentId": 2,
+        "result": [[6,2],[4,6],[7,5],[5,7],[6,1]],
+        "aces": [6,15],
+        "smashedRackets":[2,1],
+        "doubleFaults": [3,4]
+    },
+    {}
+]
+
+function calcSalary(id) {
+    console.log(id)
+
+    let totalSalary = 0
+
+    return totalSalary
+}
+
+calcSalary(1) // should be: XXX
