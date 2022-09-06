815ccf621edd03c36a6dd1c16e0826cb479b5548
Fred Snyder <safe.sand1626@fastmail.com>
2022-09-06 11:04:58 +0000

    Convert data to json import

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
 // app
 console.log('Tennis Player Salaries');
 
-// sample data
-const matches = [
-	{
-		playerId: 1,
-		opponentId: 2,
-		result: [
-			[6, 2],
-			[4, 6],
-			[7, 5],
-			[5, 7],
-			[6, 1],
-		],
-		aces: [6, 15],
-		smashedRackets: [2, 1],
-		doubleFaults: [3, 4],
-	},
-];
+import data from './data/player_data.json' assert { type: 'json' };
 
 /*
  * 500e        for every match played
